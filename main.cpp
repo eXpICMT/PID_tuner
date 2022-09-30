f01e2f2b7a895925be36bcd391989892f1a34962
unknown <vvb5p@KWS097.corp.aedon.ru>
2022-09-30 13:22:07 +0300

    Added new .db.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <QDebug>
 #include <iostream>
 
+#define DEBUG_CMD_APP
+
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -15,9 +17,14 @@ int main(int argc, char *argv[])
 	QString filename = name_log_dir + "/test.txt";
 	QFile file(filename);
 	QTextStream fileText{&file};
+
+	QByteArray datum;
+
+#ifdef DEBUG_CMD_APP
 	string strDatum {};
 	int i {};
-	QByteArray datum;
+	double sum {};
+#endif
 
 	if(!file.open(QIODevice::ReadOnly | QIODevice::ExistingOnly))
 	{
@@ -30,10 +37,13 @@ int main(int argc, char *argv[])
 			datum.clear();
 			datum = file.readLine();
 			datum = datum.trimmed();
-			strDatum = datum.toStdString();
-			qDebug() << datum;
-			cout << "V(" << i << ") = " << strDatum << endl;
+
+#ifdef DEBUG_CMD_APP
+			qDebug() << "V(" << i << ") = " << datum;
+			sum += datum.toDouble();
+			qDebug() << "SUM(V(" << (i-i) << "..." << i << ")) = " << sum;
 			i++;
+#endif
 		}
 		file.close();
 	}
--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
-## How to use it. 
+# How to use it. 
 
-# 1. Convert .db to .csv by using "Export CSV" in SQLite.
-# 2. Copy and paste target column to .txt , e.g. "Ubat" in my case.
-# 3. Rename "*.txt" to "test.txt".
-# 4. In process...
+## 1. Convert .db to .csv by using "Export CSV" in SQLite.
+## 2. Copy and paste target column to .txt , e.g. "Ubat" in my case. Target range is {111...4075} in .csv or {110...4074} .db.
+## 3. Rename "*.txt" to "test.txt".
+## 4. In process...
+## 5.
