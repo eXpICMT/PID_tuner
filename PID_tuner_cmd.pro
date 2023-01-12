ef31cfc21cd3ea10e28eeb60d9d19fa9a5b08cb0
eXpICMT <eXpICMT@yandex.ru>
2023-01-12 16:50:07 +0300

    This model is enough for the next step (try to implement Kalman filter)

--- PID_tuner_cmd.pro
+++ PID_tuner_cmd.pro
@@ -19,18 +19,11 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 DISTFILES += \
 	combined_model \
-	db_pid_tuner_sql10_22.db \
 	dups_sql09_22.db \
-	dups_sql09_22_093022_SoC.db \
-	hppc_test_from_112122_rev.3.csv \
-	hppc_test_from_112122_v2.csv \
-	main \
+	dups_sql11_22_hppc_2d_try_delta_gel_12_15.db \
 	main_tevenin_model \
-	readme.md \
-	t1_for_Up.txt \
-	t2_for_Up.txt \
-	test.csv \
-	test.txt
+	preparedTable.csv \
+	readme.md
 
 HEADERS += \
 	alphabetafilter.h \
--- main.cpp
+++ main.cpp
@@ -9,43 +9,51 @@
 #include <iostream>
 #include <QVector>
 
+/* Description
+ * The model is combination of:
+ * Shepherd model:              yk = E0 − R*ik − Ki/zk
+ * Unnewehr universal model:    yk = E0 − R*ik − Ki*zk
+ * Nernst model:                yk = E0 − R*ik + K2*ln(zk)+K3*ln(1−zk)
+ * where
+ * ik - measured current [A]
+ * zk - measured SOC
+*/
 
 /*
     Battery Model Parameters
 
     Open Circuit Voltage
 
-    Uocv_100%       = 51.2
-    Uocv_90%        = 50.31
-    Uocv_80%        = 49.46
-    Uocv_70%        = 48.49
+      Uocv_100%       = 51.2
+      Uocv_90%        = 50.31
+      Uocv_80%        = 49.46
+      Uocv_70%        = 48.49
 
-    State of Charge
+      State of Charge
 
-    SOC_100%        = 15
-    SOC_90%         = 13.5
-    SOC_80%         = 12
-    SOC_70%         = 10.5
+      SOC_100%        = 15
+      SOC_90%         = 13.5
+      SOC_80%         = 12
+      SOC_70%         = 10.5
 
-    Internal Resistance
+      Internal Resistance
 
-    Rin-_100%       = (51.2 - 47.71)/15.1
-    Rin+_100%       = (59.71 - 50.67)/7.35
+      Rin-_100%       = (51.2 - 47.71)/15.1
+      Rin+_100%       = (59.71 - 50.67)/7.35
 
-    Rin-_90%        = (50.31 - 47.1)/15.1
-    Rin+_90%        = (58.78 - 49.98)/15.86
+      Rin-_90%        = (50.31 - 47.1)/15.1
+      Rin+_90%        = (58.78 - 49.98)/15.86
 
-    Rin-_80%        = (49.46 - 46.6)/15.09
-    Rin+_80%        = (54.9 - 49.14)/15.85
+      Rin-_80%        = (49.46 - 46.6)/15.09
+      Rin+_80%        = (54.9 - 49.14)/15.85
 
-    Rin-_70%        = (48.49 - 45.71)/15.1
-    Rin+_70%        = (53.83 - 48.38)/15.85
+      Rin-_70%        = (48.49 - 45.71)/15.1
+      Rin+_70%        = (53.83 - 48.38)/15.85
 
-    float SOC = 1+(Icnt/(15*3600));
+      float SOC = 1+(Icnt/(15*3600));
 
 */
 
-//using namespace std;
 using Eigen::MatrixXf;
 using Eigen::MatrixXd;
 
@@ -82,7 +90,7 @@ int main(int argc, char *argv[]){
     DB = database::getInstance();
     DB->connectToDataBase(name_log_dir);
     int i = 0;
-    float Yest {0.0}, Ipos {0.0}, Ineg {0.0};
+    float Yest {0.0};
 
     if(!csv1.open(QIODevice::ReadOnly | QIODevice::ExistingOnly)){
         qWarning("The csv 1 wasn't read");
@@ -97,22 +105,19 @@ int main(int argc, char *argv[]){
             Itemp.push_back(Icsv);
             Ucsv = item.at(1).toDouble();
             Ytemp.push_back(Ucsv);
-            //qDebug() << "Icsv[" << i << "] = " << Itemp.last() << "   Ucsv[" << i << "] = " << Ytemp.last();
-            //SOC += Icsv/(15*3600);
-            //qDebug() << "SOC[" << i << "] = " << SOC;
             i++;
         }
     }
 
     MatrixXd Y(Ytemp.size(), 1);
-    MatrixXd H(Ytemp.size(), 6), HTH(6,6), Htransposed(6,Ytemp.size()), HTHinversed(6,6);
-    MatrixXd Theta(6, 1);
+    MatrixXd H(Ytemp.size(), 7), HTH(7,7), Htransposed(7,Ytemp.size()), HTHinversed(7,7);
+    MatrixXd Theta(7, 1);
 
     i = 0;
     while(i < Ytemp.size()){
         Y(i,0) = Ytemp.at(i);
         H(i, 0) = 1;//for K0
-        if(Itemp.at(i) > 0){
+        if(Itemp.at(i) > 0.0){
             H(i, 1) = Itemp.at(i);//for I+
             H(i, 2) = 0.0;//for I-
         }else{
@@ -120,57 +125,39 @@ int main(int argc, char *argv[]){
             H(i, 2) = Itemp.at(i);
         }
         SOC += Itemp.at(i)/(15.0*3600.0);
-        H(i, 3) = 1/SOC;
+        if(SOC > 1.0)
+            SOC = 1.0;
+        H(i, 3) = 1.0/SOC;
         H(i, 4) = SOC;
         H(i, 5) = log(SOC);
-        //H(i, 6) = log(1-SOC);
-        //std::cout << "Y(" <<i << ", 1) = " << Y(i,0) << std::endl;
+        if((1-SOC) > 0.0)
+            H(i, 6) = log(1-SOC);
+        else
+            H(i, 6) = 1.0;
         i++;
     }
+
+    //Theta = (H^T * H)^-1 * H^T * Y
+
     Htransposed = H.transpose();
-    //std::cout << "H.transpose():" << std::endl;
-    //std::cout << Htransposed << std::endl;
-
     HTH = Htransposed*H;
-    //std::cout << "HTH:" << std::endl;
-    //std::cout << HTH << std::endl;
-
     HTHinversed = HTH.inverse();
-    //std::cout << "HTH.inverse():" << std::endl;
-    //std::cout << HTHinversed << std::endl;
-
     Theta = HTHinversed*Htransposed;
-    //std::cout << "Theta:" << std::endl;
-    //std::cout << Theta << std::endl;
-
     Theta *= Y;
+
     std::cout << "Theta:" << std::endl;
     std::cout << Theta << std::endl;
 
     i = 0;
-    SOC = 1.0;
 
     while(i < Ytemp.size()){
-        float temp{0.0};
-        SOC += Itemp.at(i)/(15.0*3600.0);
-        if(Itemp.at(i) > 0){
-            Ipos = Itemp.at(i); //for I+
-            Ineg = 0.0;         //for I-
-        }else{
-            Ipos = 0.0;
-            Ineg = Itemp.at(i);
-        }
-        Yest = Theta(0,0);          //+ K0
-        Yest += Theta(1,0)*Ipos;    //+ R+
-        Yest += Theta(2,0)*Ineg;    //+ R-
-        temp = Theta(3,0);          //find K1 1st half
-        temp /= SOC;                //find K1
-        Yest += temp;               //+ K1
-        temp = Theta(4,0)*SOC;      //find K2
-        Yest += temp;               //+ K2
-        temp = Theta(5,0);          //find K3 1st half
-        temp *= log(SOC);           //find K3
-        Yest += temp;               //+ K3
+        Yest = Theta(0,0) * H(i,0);          //1
+        Yest += Theta(1,0) * H(i,1);         //2
+        Yest += Theta(2,0) * H(i,2);         //3
+        Yest += Theta(3,0) * H(i,3);         //4
+        Yest += Theta(4,0) * H(i,4);         //5
+        Yest += Theta(5,0) * H(i,5);         //6
+        Yest += Theta(6,0) * H(i,6);         //7
         dataDB.clear();
         dataDB.append(QString::number(Y(i,0)));
         dataDB.append(QString::number(Yest));
@@ -181,6 +168,17 @@ int main(int argc, char *argv[]){
         }
         i++;
     }
+
+/* Result
+    Theta:
+       51.1968
+      0.361198
+      0.341589
+       15.1891
+      -15.4414
+       37.8947
+    -0.0317025
+*/
 
     csv1.close();
     DB->closeDataBase();
